af3c32783705524bac9bfdb943470644b42eac55
linzi-jiang <130731899+linzi-jiang@users.noreply.github.com>
2023-05-26 03:45:40 +0800

    all0526

---
+++ shouye.js
@@ -0,0 +1,9 @@
+var imageContainer = document.getElementById("image-spark");
+
+function showImage() {
+  imageContainer.style.display = "block";
+}
+
+function hideImage() {
+  imageContainer.style.display = "none";
+}
